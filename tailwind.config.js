4fad5e3d163f8b7a2ab5dbc8f3d64970849d1e84
NormanThohir <normanmuhammad300@gmail.com>
2025-12-13 19:37:16 +0900

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./src/**/*.html"], // agar Tailwind scan HTML kita
+    theme: {
+      extend: {
+        fontFamily: {
+          inter: ['Inter', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
